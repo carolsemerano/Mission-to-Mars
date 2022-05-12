6ba8943c1a12571bcac6d0a56f7fa92f35baef7d
Carolina Semerano <carol_semerano@live.com>
2022-05-12 18:08:09 -0400

    applying bootstrap

--- scraping.py
+++ scraping.py
@@ -97,7 +97,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes=["table", "table-condensed"])
 
 def hemisphere(browser):
     # 1. Use browser to visit the URL 
